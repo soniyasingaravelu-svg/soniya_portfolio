ad6d460d96d0abc37a0a32554434f3eb1df313e8
Soniya S <soniyasingaravelu@gmail.com>
2026-08-25 16:49:34 +0530

    Remove unused author rename script

--- rename_author.py
+++
@@ -1,8 +0,0 @@
-def commit_callback(commit):
-    if commit.author_email == b"samkishore16@gmail.com":
-        commit.author_name = b"Soniya"
-        commit.author_email = b"soniyasingaravelu@gmail.com"
-
-    if commit.committer_email == b"samkishore16@gmail.com":
-        commit.committer_name = b"Soniya"
-        commit.committer_email = b"soniyasingaravelu@gmail.com"
